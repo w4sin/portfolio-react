ed710bb9e326555f6d9249eebede3582d84a92ea
Wasin Saetie <wasin.saetie@gmail.com>
2025-05-13 17:54:08 +0700

    feat: integrate persistence for billing store.

--- src/state-management/billing-store.ts
+++ src/state-management/billing-store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { TProductBilling } from '../types/product';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 type BillingState = {
     items: TProductBilling[];
@@ -8,28 +9,39 @@ type BillingState = {
     clearItems: () => void;
 }
 
-export const useBillingStore = create<BillingState>()((set) => ({
-    items: [],
-    addItem: (item) => set((state) => {
-        const existingItem = state.items.find((i) => i.id === item.id);
-        if (existingItem) {
-            return {
-                items: state.items.map((i) => {
-                    if (i.id === item.id) {
-                        if (i.quantity + item.quantity <= 0)
-                            return { ...i, quantity: 0 };
-                        if (i.quantity + item.quantity > 99)
-                            return { ...i, quantity: 99 };
-                        return { ...i, quantity: i.quantity + item.quantity };
-                    } else
-                        return i;
+export const useBillingStore = create<BillingState>()(
+    persist(
+        (set, get) =>
+        ({
+            items: [],
+            addItem: (item) => set((state) => {
+                const { items } = state;
+                const existingItem = items.find((i) => i.id === item.id);
+                if (existingItem) {
+                    return {
+                        items: items.map((i) => {
+                            if (i.id === item.id) {
+                                if (i.quantity + item.quantity <= 0)
+                                    return { ...i, quantity: 0 };
+                                if (i.quantity + item.quantity > 99)
+                                    return { ...i, quantity: 99 };
+                                return { ...i, quantity: i.quantity + item.quantity };
+                            } else
+                                return i;
+                        }
+                        ),
+                    };
                 }
-                ),
-            };
+                return { items: [...items, item] };
+            }
+            ),
+            removeItem: (itemId) => set({ items: get().items?.filter((item) => item.id !== itemId) }),
+            clearItems: () => set({ items: [] }),
+        }),
+        {
+            name: 'billing-storage',
+            storage: createJSONStorage(() => sessionStorage),
         }
-        return { items: [...state.items, item] };
-    }
-    ),
-    removeItem: (itemId) => set((state) => ({ items: state.items.filter((item) => item.id !== itemId) })),
-    clearItems: () => set({ items: [] }),
-}));
+    )
+);
+
